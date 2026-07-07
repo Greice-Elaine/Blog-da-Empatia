1635f2c6d00fc44df3438aa54ecf12071ea742e8
Greice-Elaine <greice.baciquette@escola.pr.gov.br>
2026-07-07 15:12:56 -0300

    Add function to display empathy message

---
+++ script.js
@@ -0,0 +1,7 @@
+function mostrarMensagem(){
+
+    document.getElementById("mensagemFinal").innerHTML =
+
+    "💙 A empatia começa com um simples gesto de respeito. Seja a mudança que você deseja ver na escola! 💙";
+
+}
